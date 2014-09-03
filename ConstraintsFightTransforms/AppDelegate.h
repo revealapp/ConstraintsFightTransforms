93ffb0ebbf5abac1829927b7c0561f4048b692f1
Vlas Voloshin <argentumko@gmail.com>
2014-09-03 18:37:23 +1000

    added gitignore, updated copyright notices, removed some cruft

--- ConstraintsFightTransforms/AppDelegate.h
+++ ConstraintsFightTransforms/AppDelegate.h
@@ -1,8 +1,4 @@
 //
-//  AppDelegate.h
-//  ConstraintsFightTransforms
-//
-//  Created by Adam Eberbach on 11/07/2014.
 //  Copyright (c) 2014 Itty Bitty Apps. All rights reserved.
 //
 
--- ConstraintsFightTransforms/TransformsViewController.m
+++ ConstraintsFightTransforms/TransformsViewController.m
@@ -1,3 +1,4 @@
+//
 //  Copyright (c) 2014 Itty Bitty Apps. All rights reserved.
 //
 
